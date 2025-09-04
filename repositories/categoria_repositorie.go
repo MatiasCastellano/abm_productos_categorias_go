67ddd4554b3c9c626946f74d75625a76af62b2ed
Matias <matiascastellano93@gmail.com>
2025-09-04 09:57:50 -0300

    repositories

--- repositories/categoria_repositorie.go
+++ repositories/categoria_repositorie.go
@@ -1,1 +1,82 @@
 package repositories
+
+import (
+	"abm_productos_categorias_go/database"
+	"abm_productos_categorias_go/model"
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type CategoriaRepositorioInterface interface {
+	CrearCategoria(categoria model.Categoria) (*mongo.InsertOneResult, error)
+	EliminarCategoria(Id primitive.ObjectID) (*mongo.DeleteResult, error)
+	ActualizarCategoria(categoria model.Categoria) (*mongo.UpdateResult, error)
+	ObtenerCategorias(nombre string) ([]model.Categoria, error)
+	ObtenerCategoriaPorId(id primitive.ObjectID) (model.Categoria, error)
+}
+
+type CategoriaRepositorio struct {
+	db database.DB
+}
+
+func NuevoCategoriaRepositorio(db database.DB) *CategoriaRepositorio {
+	return &CategoriaRepositorio{db: db}
+}
+
+func (repositorio CategoriaRepositorio) CrearCategoria(categoria model.Categoria) (*mongo.InsertOneResult, error) {
+	colecion := repositorio.db.GetClient().Database("abm_productos").Collection("categorias")
+	resultado, err := colecion.InsertOne(context.TODO(), categoria)
+	return resultado, err
+}
+
+func (repositorio CategoriaRepositorio) EliminarCategoria(id primitive.ObjectID) (*mongo.DeleteResult, error) {
+	colecion := repositorio.db.GetClient().Database("abm_productos").Collection("categorias")
+	filtro := bson.M{"_id": id}
+	resultado, err := colecion.DeleteOne(context.TODO(), filtro)
+	return resultado, err
+}
+
+func (repositorio CategoriaRepositorio) ActualizarCategoria(categoria model.Categoria) (*mongo.UpdateResult, error) {
+	colecion := repositorio.db.GetClient().Database("abm_productos").Collection("categorias")
+	filtro := bson.M{"_id": categoria.ID}
+	actualizacion := bson.M{
+		"$set": bson.M{
+			"nombre":      categoria.Nombre,
+			"descripcion": categoria.Descripcion}}
+	resultado, err := colecion.UpdateOne(context.TODO(), filtro, actualizacion)
+	return resultado, err
+}
+
+func (repositorio CategoriaRepositorio) ObtenerCategorias(nombre string) ([]model.Categoria, error) {
+	colecion := repositorio.db.GetClient().Database("abm_productos").Collection("categorias")
+	filtro := bson.M{}
+	if nombre != "" {
+		filtro[nombre] = bson.M{"$regex": nombre, "$options": "i"}
+	}
+	cursor, err := colecion.Find(context.TODO(), filtro)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.Background())
+	var categorias []model.Categoria
+	for cursor.Next(context.Background()) {
+		var categoria model.Categoria
+		err := cursor.Decode(&categoria)
+		if err != nil {
+			continue
+		}
+		categorias = append(categorias, categoria)
+	}
+	return categorias, nil
+}
+
+func (repositorio ProductoRepositorio) ObtenerCategoriaPorId(id primitive.ObjectID) (model.Categoria, error) {
+	colecion := repositorio.db.GetClient().Database("abm_productos").Collection("categorias")
+	filtro := bson.M{"_id": id}
+	var categoria model.Categoria
+	err := colecion.FindOne(context.TODO(), filtro).Decode(&categoria)
+	return categoria, err
+}
--- repositories/producto_repositorie.go
+++ repositories/producto_repositorie.go
@@ -1,1 +1,87 @@
 package repositories
+
+import (
+	"abm_productos_categorias_go/database"
+	"abm_productos_categorias_go/model"
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type ProductoRepositorioInterface interface {
+	CrearProducto(producto model.Producto) (*mongo.InsertOneResult, error)
+	EliminarProducto(Id primitive.ObjectID) (*mongo.DeleteResult, error)
+	ActualizarProducto(producto model.Producto) (*mongo.UpdateResult, error)
+	ObtenerProductos(nombre string, precio float64) ([]model.Producto, error)
+	ObtenerProductoPorId(id primitive.ObjectID) (model.Producto, error)
+}
+
+type ProductoRepositorio struct {
+	db database.DB
+}
+
+func NuevoProductoRepositorio(db database.DB) *ProductoRepositorio {
+	return &ProductoRepositorio{db: db}
+}
+
+func (repositorio ProductoRepositorio) CrearProducto(producto model.Producto) (*mongo.InsertOneResult, error) {
+	colleccion := repositorio.db.GetClient().Database("abm_productos").Collection("productos")
+	resultado, err := colleccion.InsertOne(context.TODO(), producto)
+	return resultado, err
+}
+
+func (repositorio ProductoRepositorio) EliminarProducto(id primitive.ObjectID) (*mongo.DeleteResult, error) {
+	colleccion := repositorio.db.GetClient().Database("abm_productos").Collection("productos")
+	filtro := bson.M{"_id": id}
+	resultado, err := colleccion.DeleteOne(context.TODO(), filtro)
+	return resultado, err
+}
+
+func (repositorio ProductoRepositorio) ActualizarProducto(producto model.Producto) (*mongo.UpdateResult, error) {
+	coleccion := repositorio.db.GetClient().Database("abm_productos").Collection("productos")
+	filtro := bson.M{"_id": producto.ID}
+	actualizacion := bson.M{
+		"$set": bson.M{
+			"categoria_id": producto.CategoriaId,
+			"nombre":       producto.Nombre,
+			"precio":       producto.Precio,
+			"descripcion":  producto.Descripcion}}
+	resultado, err := coleccion.UpdateOne(context.TODO(), filtro, actualizacion)
+	return resultado, err
+}
+
+func (repositorio ProductoRepositorio) ObtenerProductos(nombre string, precio float64) ([]model.Producto, error) {
+	coleccion := repositorio.db.GetClient().Database("abm_productos").Collection("productos")
+	filtro := bson.M{}
+	if nombre != "" {
+		filtro[nombre] = bson.M{"$regex": nombre, "$options": "i"}
+	}
+	if precio >= 0 {
+		filtro["precio"] = bson.M{"$gt": precio}
+	}
+	cursor, err := coleccion.Find(context.TODO(), filtro)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(context.Background())
+	var productos []model.Producto
+	for cursor.Next(context.Background()) {
+		var producto model.Producto
+		err := cursor.Decode(&producto)
+		if err != nil {
+			continue
+		}
+		productos = append(productos, producto)
+	}
+	return productos, nil
+}
+
+func (repositorio ProductoRepositorio) ObtenerProductoPorId(id primitive.ObjectID) (model.Producto, error) {
+	coleccion := repositorio.db.GetClient().Database("abm_productos").Collection("productos")
+	filtro := bson.M{"_id": id}
+	var producto model.Producto
+	err := coleccion.FindOne(context.TODO(), filtro).Decode(&producto)
+	return producto, err
+}
